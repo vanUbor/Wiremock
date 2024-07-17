156a203c6d9b3af193dd11a879ed2ea6a9321a11
vanUbor <vanUbor@live.de>
2024-07-17 16:54:41 +0200

    Testing Github by adding a failing test

--- WireMock.Test/TestTests.cs
+++ WireMock.Test/TestTests.cs
@@ -1,11 +1,17 @@
 namespace WireMock.Test;
 
 [TestClass]
-public class UnitTest1
+public class TestTests
 {
     [TestMethod]
     public void AlwaysTrue()
     {
         Assert.IsTrue(true);
+    }
+    
+    [TestMethod]
+    public void AlwaysFails()
+    {
+        Assert.IsTrue(false);
     }
 }
