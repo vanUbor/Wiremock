e7e7c6855e3d34352c553b93f6d37cf3ccebee18
Locke vanUbor <vanUbor@live.de>
2024-08-18 19:13:44 +0200

    Refactor `ChangedMappingsEventArgs` to a separate file
    Extracted the `ChangedMappingsEventArgs` class from WireMockService.cs to its own file, `ChangedMappingsEventArgs.cs`. This improves code organization and modularity, making the codebase easier to manage and navigate.

---
+++ WireMock/Server/ChangedMappingsEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using WireMock.Admin.Mappings;
+
+namespace WireMock.Server;
+
+public class ChangedMappingsEventArgs(IList<MappingModel> mappingModels) : EventArgs
+{
+    public required string ServiceId { get; init; }
+    public IList<MappingModel> MappingModels { get; } = mappingModels;
+}
--- WireMock/Server/WireMockService.cs
+++ WireMock/Server/WireMockService.cs
@@ -117,9 +117,3 @@ public class WireMockService(WireMockServiceModel model)
 
  
 }
-
-public class ChangedMappingsEventArgs(IList<MappingModel> mappingModels) : EventArgs
-{
-    public required string ServiceId { get; init; }
-    public IList<MappingModel> MappingModels { get; } = mappingModels;
-}
