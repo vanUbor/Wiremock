8cacfe65551de67cd2d8f8e3fb9e3e76176fe9a3
Locke vanUbor <vanUbor@live.de>
2024-08-23 20:43:57 +0200

    Enhance security and code readability
    Added 'rel="noopener"' to external links for improved security in _Layout.cshtml. Renamed parameters in IOrchestrator interface methods for better clarity and consistency.

--- WireMock/Server/Interfaces/IOrchestrator.cs
+++ WireMock/Server/Interfaces/IOrchestrator.cs
@@ -2,10 +2,10 @@
 
 public interface IOrchestrator
 {
-    void RemoveService(int id);
-    void Stop(int id);
-    Task CreateServiceAsync(int id);
+    void RemoveService(int serviceId);
+    void Stop(int serviceId);
+    Task CreateServiceAsync(int serviceId);
     Task<IList<WireMockService>> GetOrCreateServicesAsync();
-    Task StartServiceAsync(int id);
+    Task StartServiceAsync(int serviceId);
     bool IsRunning(int serviceId);
 }
