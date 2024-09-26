a97b930e0ec801d1707da0fa93b4f2b3fcdae9e6
Locke vanUbor <vanUbor@live.de>
2024-09-26 21:48:13 +0200

    Update mappings UI and add TypeScript foobar module
    Renamed a button label in Mappings.cshtml from "Details and Edit" to "Show Json" for clarity. Added a new TypeScript file 'foobar.ts' and its compiled JavaScript version 'foobar.cjs'. Updated TypeScript compiler options and added @types/node dependency, also upgraded rollup optional dependencies.

---
+++ WireMockUI/src/foobar.ts
@@ -0,0 +1,26 @@
+﻿import {promises as fs} from 'fs';
+
+async function readFile(): Promise<string> {
+
+    return new Promise<string>((resolve, reject) => {
+        try {
+            console.log('Reading file...');
+            let data = fs.readFile('../Scratches/mapping.json', 'utf-8');
+            data.then(json => {
+                if (json.charCodeAt(0) === 0xFEFF) {
+                    resolve(json.slice(1));
+                } else {
+                    resolve(json);
+                }
+            });
+        }catch (error) {
+            reject('Error reading the file: ' + error);
+        }
+    });
+}
+
+let p = readFile();
+p.then((result) => {
+    let o : any = JSON.parse((result))
+    console.log(o);
+});
